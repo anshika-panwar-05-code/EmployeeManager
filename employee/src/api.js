621110842f8728f08ad9f34132f6bde41b596b50
Anshika Panwar <anshikapanwar74@gmail.com>
2025-08-04 20:55:42 +0530

    Update api.js

--- employee/src/api.js
+++ employee/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API= axios.create({
-    baseURL:"http://localhost:3000/api"
+    baseURL:"https://employeemanager-2j8v.onrender.com/api"
 })
 
 export default API;
